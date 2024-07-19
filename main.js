c217061db6495c4fd1cabf15d696713eb50598f7
akitaburhan <burhan.kilic@akitarosewater.ca>
2024-07-19 11:21:32 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// Assign a variable to a string value
+var myString = "Hello, this is a message from JavaScript!";
+
+// Use the document.write() method to display the variable
+document.write(myString);
