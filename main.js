5f3742aab9415e69622fa869b5e5efeefc031bda
akitaburhan <burhan.kilic@akitarosewater.ca>
2024-07-19 11:25:02 -0400

    Update main.js
    window.alert("This is an alert from JavaScript!");

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ var myString = "Hello, this is a message from JavaScript!";
 
 // Use the document.write() method to display the variable
 document.write(myString);
+
+window.alert("This is an alert from JavaScript!");
